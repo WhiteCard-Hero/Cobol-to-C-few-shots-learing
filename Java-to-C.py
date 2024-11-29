0eb4b3c57a78242fe9376250576722e6ed787607
Willis~ <willischeung2004@gmail.com>
2024-11-29 12:21:56 +0800

    finetune.py from hugging face

--- Java-to-C.py
+++ Java-to-C.py
@@ -1,4 +1,13 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import matplotlib.pyplot as plt
+import torch
 
-print()
+if __name__ == "__main__" :
+    # path = './' + input('filename: ')
+    # file = open(path)
+    model = AutoModelForCausalLM.from_pretrained('bigcode/starcoder2-3b', device_map='auto')
+    tokenizer = AutoTokenizer.from_pretrained('bigcode/starcoder2-3b')
+
+    tokenized_input = tokenizer( 'Hello world in Java', return_tensors='pt' ).to('cuda')
+    generated_ids = model.generate( **tokenized_input )
+    print(tokenizer.batch_decode(generated_ids)[0])
